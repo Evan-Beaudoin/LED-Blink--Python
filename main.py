4c694edef6b3c816d948c022d81ae5aa9450e213
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-16 19:07:09 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#Created by: Evan
+#Created on: Oct. 2020
+
+#This program blinks and LED
+
+from microbit import *
+
+LED = pin0
+
+while True:
+   LED.write_digital(1)
+   sleep(1000)
+   LED.write_digital(0)
+   sleep(1000)
+
